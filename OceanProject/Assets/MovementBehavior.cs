c05768b21984fc8b5c8262d7b902df443d5303d4
Micnasr <micraidz@gmail.com>
2020-06-25 14:32:54 -0400

    newmovement

--- OceanProject/Assets/MovementBehavior.cs
+++ OceanProject/Assets/MovementBehavior.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class MovementBehavior : MonoBehaviour
 {
-    public float speed;
+    public float Waterspeed;
 
     private Rigidbody2D rb;
     private Vector2 moveVelocity;
 
     public bool InWater = false;
+
+    public float Landspeed;
+    public float jumpForce;
+    private float moveInput;
+
+    private bool facingRight = true;
+
+    private bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+
+    private int extraJumps;
+    public int extraJumpsValue;
+
+
 
     void Start()
     {
@@ -20,23 +36,58 @@ public class MovementBehavior : MonoBehaviour
     void Update()
     {
         Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        moveVelocity = moveInput * speed;
+        moveVelocity = moveInput * Waterspeed;
 
         CheckStatus();
+
+        if (isGrounded == true)
+        {
+            extraJumps = extraJumpsValue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.W) && extraJumps == 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+        
     }
 
     void FixedUpdate()
     {
-        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+       
 
         if (InWater)
         {
             rb.gravityScale = 0;
+            rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
         } else
         {
-            rb.gravityScale = 9.8f;
+            rb.gravityScale = 1f;
+            isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+
+
+
+            moveInput = Input.GetAxis("Horizontal");
+            rb.velocity = new Vector2(moveInput * Landspeed, rb.velocity.y);
         }
+
         
+
+        if (facingRight == false && moveInput > 0)
+        {
+            Flip();
+        }
+        else if (facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
     }
 
     public void CheckStatus()
@@ -49,4 +100,15 @@ public class MovementBehavior : MonoBehaviour
             InWater = false;
         }
     }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
 }
+
+
+
